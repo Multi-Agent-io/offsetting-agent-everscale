3eae29ee9bae0484ea3446a576bf94a3c1c46134
tubleronchik <m.bystramovich@gmail.com>
2022-12-09 17:33:19 +0300

    add function to interact with contract

--- agent.js
+++ agent.js
@@ -1,19 +1,55 @@
 "use strict"
-import config from './config/config.json' assert { type: 'json' };
+import { config } from './config/config.js';
+
+import BigNumber from "bignumber.js";
 import * as IPFS from 'ipfs-core'
+import { TonClient, abiContract, signerKeys, signerNone } from '@eversdk/core';
+import { libNode } from '@eversdk/lib-node';
+import { AssetFactory } from './contracts/AssetFactory.js';
+
+const ipfs = await IPFS.create({ repo: "ipfs" })
+const ENDPOINTS = ['https://devnet-sandbox.evercloud.dev/graphql']
+const { ipfs_topic, auditor_address_config, auditor_seed } = config
+
+TonClient.useBinaryLibrary(libNode);
+const client = new TonClient({
+    network: {
+        endpoints: ENDPOINTS
+    },
+});
+
+const keyPair = await client.crypto.mnemonic_derive_sign_keys({
+    phrase: auditor_seed
+});
+console.log(`Generated key pair:`);
+console.log(keyPair);
+
+const assetFactoryAddress = '0:05e30bf3eae57adf9f5b6e45d4f55ffbb0dbf6e9b8d7441a67167631ce2675eb'
+const userWalletAdress = "0:8378231949d0945553926f0fd4798c48bcfb4343bea528b6f6383e6bd1b8e4ba"
 
 
-const ipfs = await IPFS.create({ repo: "~/work" })
 
-let receiveMsg = (msg) => {
-    let data = JSON.parse(new TextDecoder().decode(msg.data))
-    if (data["auditor"] === config["auditor_address"]) {
-        console.log(data)
-        let assets_to_mint = assets_calculation(data["log"])
-        let token_name = data["token_name"]
-        let token_symbol = data["token_symbol"]
+async function receiveMsg (msg) {
+    try {
+        const { log, token_name, token_symbol, auditor } = JSON.parse(new TextDecoder().decode(msg.data))
+        console.log(auditor)
+        let convertedAddress = (await client.utils.convert_address({
+            address: auditor,
+            output_format: {
+                type: "AccountId"
+            },
+        })).address;
+        console.log(`Address in HEX format: ${convertedAddress}`)
 
-    }
+        if (auditor === auditor_address_config) {
+            let assets_to_mint = assets_calculation(log)
+            console.log(assets_to_mint)
+            mintAsset(assets_to_mint, auditor)
+        }
+      } catch (error) {
+        console.error(error)
+        return
+      }
 }
 
 function assets_calculation(log) {
@@ -25,7 +61,48 @@ function assets_calculation(log) {
     return total_assets
 }
 
+async function mintAsset(amount, auditor_address) {
+    const deployRootParams = {
+        send_events: false,
+        message_encode_params: {
+            address: assetFactoryAddress,
+            abi: {
+                type: 'Contract',
+                value: AssetFactory.abi,
+            },
+            call_set: {
+                function_name: 'deployRoot',
+                input: {
+                    answerId: 0,
+                    name: "TestAsset",
+                    symbol: "TST",
+                    decimals: 9,
+                    owner: auditor_address,
+                    initialSupplyTo: userWalletAdress,
+                    initialSupply: amount * 10 ** 9,
+                    deployWalletValue: new BigNumber(0.2).shiftedBy(9).toFixed(0),
+                    mintDisabled: false,
+                    burnByRootDisabled: false,
+                    burnPaused: false,
+                    remainingGasTo: auditor_address,
+                    upgradeable: false
+                },
+            },
+            signer: signerKeys(keyPair),
+        },
+    };
+    console.log(`Minting ${amount} tokens`);
+    // Call `sendValue` function
+    try {
+        const response = await client.processing.process_message(deployRootParams);
+        console.log(response.transaction);
+    } catch(error) {
+        console.log(error)
+        console.log(error.data)
+    }
+
+    // return response.transaction.lt;
+}
 
 
-await ipfs.pubsub.subscribe(config["ipfs_topic"], receiveMsg)
-
+await ipfs.pubsub.subscribe(ipfs_topic, receiveMsg)
---
+++ config/config_example.js
@@ -0,0 +1,6 @@
+export const config = {
+    "ipfs_provider": "/ip4/127.0.0.1/tcp/5001/http",
+    "ipfs_topic": "offsetting.service.everscale",
+    "auditor_address_config": "0:0000000000000000000000000000000000000000000000000000000000000000",
+    "auditor_seed": ""
+}
