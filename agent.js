1d104dd977c583109536ac0e1ef3ebb5c18b7516
tubleronchik <m.bystramovich@gmail.com>
2022-12-13 01:49:30 +0300

    fix contract's msg. update artifacts

--- agent.js
+++ agent.js
@@ -1,9 +1,19 @@
-import BigNumber from 'bignumber.js';
-import * as IPFS from 'ipfs-core';
-import { TonClient } from '@eversdk/core';
+/* eslint-disable import/extensions */
+import { Account } from '@eversdk/appkit';
+import {
+  TonClient,
+  signerNone,
+  MessageBodyType,
+} from '@eversdk/core';
 import { libNode } from '@eversdk/lib-node';
+import BigNumber from 'bignumber.js';
+import { Address } from 'everscale-inpage-provider';
+import { ResponseType } from '@eversdk/core/dist/bin.js';
+import * as IPFS from 'ipfs-core';
+
+import { AssetFactoryContract } from './artifacts/AssetFactory.js';
+import { EverwalletContract } from './artifacts/EverwalletContract.js';
 import { config } from './config/config.js';
-import { AssetFactory } from './contracts/AssetFactory.js';
 
 const ipfs = await IPFS.create({ repo: 'ipfs' });
 const ENDPOINTS = ['https://devnet-sandbox.evercloud.dev/graphql'];
@@ -13,8 +23,6 @@ TonClient.useBinaryLibrary(libNode);
 const client = new TonClient({
   network: {
     endpoints: ENDPOINTS,
-    message_processing_timeout: 80000,
-    wait_for_timeout: 80000,
   },
 });
 
@@ -22,8 +30,13 @@ const client = new TonClient({
 const auditorKeyPair = await client.crypto.mnemonic_derive_sign_keys({
   phrase: auditorSeed,
 });
-const assetFactoryAddress = '0:05e30bf3eae57adf9f5b6e45d4f55ffbb0dbf6e9b8d7441a67167631ce2675eb';
-const userWalletAdress = '0:8378231949d0945553926f0fd4798c48bcfb4343bea528b6f6383e6bd1b8e4ba';
+const assetFactoryAddress = new Address('0:05e30bf3eae57adf9f5b6e45d4f55ffbb0dbf6e9b8d7441a67167631ce2675eb');
+
+const assetFactoryContract = new Account(AssetFactoryContract, {
+  signer: signerNone(),
+  client,
+  initData: {},
+});
 
 function assetsCalculation(log) {
   /**
@@ -34,72 +47,96 @@ function assetsCalculation(log) {
   return totalAssets;
 }
 
-async function mintAsset(amount, auditorAddress) {
-  const paramsOfEncodeInternalMessage = {
-    address: assetFactoryAddress,
-    value: new BigNumber(4).shiftedBy(9).toFixed(0),
-    src_address: auditorAddress,
-    abi: {
-      type: 'Contract',
-      value: AssetFactory.abi,
-    },
+async function mintAsset(amount, auditor, tokenName, tokenSymbol, user) {
+  console.log(`Minting ${amount} tokens for ${user}`);
+  const auditorAddress = new Address(auditor)
+  const userAddress = new Address(user)
+  const inputDeployRoot = {
+    answerId: 0,
+    name: tokenName,
+    symbol: tokenSymbol,
+    decimals: 9,
+    owner: auditorAddress,
+    initialSupplyTo: userAddress,
+    initialSupply: amount * 10 ** 9,
+    deployWalletValue: new BigNumber(0.2).shiftedBy(9).toFixed(0),
+    mintDisabled: false,
+    burnByRootDisabled: false,
+    burnPaused: false,
+    remainingGasTo: auditorAddress,
+    upgradeable: false,
+  };
+
+  const deployRootMessage = (await client.abi.encode_message_body({
+    abi: assetFactoryContract.abi,
     call_set: {
       function_name: 'deployRoot',
-      input: {
-        answerId: 0,
-        name: 'TestAsset',
-        symbol: 'TST',
-        decimals: 9,
-        owner: auditorAddress,
-        initialSupplyTo: userWalletAdress,
-        initialSupply: amount * 10 ** 9,
-        deployWalletValue: new BigNumber(0.2).shiftedBy(9).toFixed(0),
-        mintDisabled: false,
-        burnByRootDisabled: false,
-        burnPaused: false,
-        remainingGasTo: auditorAddress,
-        upgradeable: false,
+      input: inputDeployRoot,
+    },
+    is_internal: true,
+    signer: signerNone(),
+  })).body;
+
+  const messageParams = {
+    send_events: false,
+    message_encode_params: {
+      address: auditorAddress,
+      abi: {
+        type: 'Contract',
+        value: EverwalletContract.abi,
+      },
+      call_set: {
+        function_name: 'sendTransaction',
+        input: {
+          dest: assetFactoryAddress,
+          value: amount * 10 ** 9,
+          bounce: true, // Send funds back on any error in desctination account
+          flags: 1, // Pay delivery fee from the multisig balance, not from the value.
+          payload: deployRootMessage,
+        },
+      },
+      signer: {
+        type: 'Keys',
+        keys: auditorKeyPair,
       },
     },
   };
-  console.log(`Minting ${amount} tokens`);
-  try {
-    const internalMessage = await client.abi.encode_internal_message(paramsOfEncodeInternalMessage);
-    const paramsOfSendMessage = {
-      message: internalMessage.message,
-      abi: {
-        type: 'Contract',
-        value: AssetFactory.abi,
+
+  const messageSubscription = await client.net.subscribe_collection({
+    collection: 'messages',
+    filter: {
+      src: { eq: assetFactoryAddress },
+      OR: {
+        dst: { eq: assetFactoryAddress },
       },
-      send_events: false,
-    };
-    const resultOfSendMessage = await client.processing.send_message(paramsOfSendMessage);
-    const paramsOfWaitForTransaction = {
-      message: internalMessage.message,
-      abi: {
-        type: 'Contract',
-        value: AssetFactory.abi,
-      },
-      send_events: false,
-      shard_block_id: resultOfSendMessage.shard_block_id,
-      sending_endpoints: resultOfSendMessage.sending_endpoints,
-    };
-    const result = await client.processing.wait_for_transaction(paramsOfWaitForTransaction);
-    console.log(result);
-  } catch (error) {
-    console.log(error);
-    console.log(error.data);
-  }
-  // return response.transaction.lt;
+    },
+    result: 'boc msg_type id src dst',
+  }, async (params, responseType) => {
+    try {
+      if (responseType === ResponseType.Custom) {
+        const { boc } = params.result;
+        const decoded = (await client.abi.decode_message({
+          abi: assetFactoryContract.abi,
+          message: boc,
+        }));
+        if (decoded.body_type === MessageBodyType.Event) {
+          console.log(`New Asset deployed at ${decoded.value.root}`);
+        }
+      }
+    } catch (err) {}
+  });
+
+  const result = await client.processing.process_message(messageParams);
+  await client.net.unsubscribe(messageSubscription);
 }
 
 async function receiveMsg(msg) {
   try {
     // eslint-disable-next-line no-unused-vars
-    const { log, tokenName, tokenSymbol, auditor } = JSON.parse(new TextDecoder().decode(msg.data));
+    const { log, tokenName, tokenSymbol, auditor, user } = JSON.parse(new TextDecoder().decode(msg.data));
     if (auditor === auditorAddressConfig) {
       const assetsToMint = assetsCalculation(log);
-      mintAsset(assetsToMint, auditor);
+      mintAsset(assetsToMint, auditor, tokenName, tokenSymbol, user);
     }
   } catch (error) {
     console.error(error);
@@ -108,7 +145,3 @@ async function receiveMsg(msg) {
 }
 
 await ipfs.pubsub.subscribe(ipfsTopic, receiveMsg);
-
-// for testing
-const msg = new TextEncoder().encode('{"auditor": "0:22d976b931c4686dc1eba73bf72dfb618bdf869b5bac77c0025af7917a3571db", "log": "Qm", "tokenName": "TEST", "tokenSymbol": "TESTSymbol"}');
-await ipfs.pubsub.publish(ipfsTopic, msg);
--- config/config_example.js
+++ config/config_example.js
@@ -1,6 +1,7 @@
+// eslint-disable-next-line import/prefer-default-export
 export const config = {
-    "ipfsProvider": "/ip4/127.0.0.1/tcp/5001/http",
-    "ipfsTopic": "offsetting.service.everscale",
-    "auditorAddressConfig": "0:0000000000000000000000000000000000000000000000000000000000000000",
-    "auditorSeed": ""
-}
+  'ipfsProvider': '/ip4/127.0.0.1/tcp/5001/http',
+  'ipfsTopic': 'offsetting.service.everscale',
+  'auditorAddressConfig': '0:0000000000000000000000000000000000000000000000000000000000000000',
+  'auditorSeed': ''
+};
